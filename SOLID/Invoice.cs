3fe6ee386632aa2204a2f4023b7d5c3e78580a22
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-21 13:15:10 +0200

    add: invoice and line item class

---
+++ SOLID/Invoice.cs
@@ -0,0 +1,27 @@
+
+namespace SOLID;
+
+public class Invoice
+{
+  public IEnumerable<LineItem> LineItems { get; set; }
+  public string Vendor { get; set; }
+  public string Vendee { get; set; }
+  public float Total { get; set; }
+
+  public Invoice(IEnumerable<LineItem> lineItems, string vendor, string vendee)
+  {
+    LineItems = lineItems;
+    Vendor = vendor;
+    Vendee = vendee;
+    Total = CalculateTotal();
+  }
+  public float CalculateTotal()
+  {
+    float total = 0;
+    foreach (var lineItem in LineItems)
+    {
+      total += lineItem.Price * lineItem.Count * (1 + lineItem.TaxRate);
+    }
+    return total;
+  }
+}
---
+++ SOLID/LineItem.cs
@@ -0,0 +1,9 @@
+namespace SOLID;
+
+public class LineItem
+{
+  public string Name { get; set; }
+  public float Price { get; set; }
+  public float Count { get; set; }
+  public int TaxRate { get; set; }
+}
